99fca66561c8434ccb972da964f0e37a882c7c93
harish <harish.chilamanthula@gmail.com>
2019-02-15 13:55:05 +0530

    done

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.get('/karvywebhook', function (req, res) {
 
 app.post('/karvymailkootwebhook', function (req, res) {
   console.log('Karvy Mailkoot logs');
-  let webhookData = JSON.parse(req.body);
+  let webhookData = req.body;
   console.log(webhookData);
   if(webhookData.event_type == "delivery_attempt" && webhookData.email == "jagapathi@gamooga.com"){
     console.log('Delivery event is done');
