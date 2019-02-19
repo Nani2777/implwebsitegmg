652069a544b623a3cf9750a6aba1abebd795ccbd
Harish <harish.chilamanthula@gmail.com>
2019-02-19 19:27:58 +0530

    removed directory of logger

--- logger.js
+++ logger.js
@@ -9,15 +9,15 @@ var Logger = exports.Logger = {};
 // Create 3 sets of write streams for the 3 levels of logging we wish to do
 // every time we get an error we'll append to our error streams, any debug message
 // to our debug stream etc...
-var infoStream = fs.createWriteStream('logs/info.txt');
+var infoStream = fs.createWriteStream('./info.txt');
 // Notice we set the path of our log files in the first parameter of 
 // fs.createWriteStream. This could easily be pulled in from a config
 // file if needed.
-var errorStream = fs.createWriteStream('logs/error.txt');
+var errorStream = fs.createWriteStream('./error.txt');
 // createWriteStream takes in options as a second, optional parameter
 // if you wanted to set the file encoding of your output file you could
 // do so by setting it like so: ('logs/debug.txt' , { encoding : 'utf-8' });
-var debugStream = fs.createWriteStream('logs/debug.txt');
+var debugStream = fs.createWriteStream('./debug.txt');
 
 
 // Finally we create 3 different functions
