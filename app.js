a322bc8507b1368984bd596b51766e517522830b
harish <harish.chilamanthula@gmail.com>
2019-03-12 16:35:14 +0530

    mailkoot data pushed

--- app.js
+++ app.js
@@ -246,17 +246,14 @@ app.post('/karvymailkootwebhook', function (req, res) {
   let webhookData = req.body;
   if (typeof (webhookData) == 'object') {
     console.log(webhookData);
-    if (webhookData['event_type'] == "delivery_attempt" || webhookData['event_type'] == "bounce_all") {
+    if ((webhookData['event_type'] == "delivery_attempt" && webhookData["status"]=="success") || webhookData['event_type'] == "bounce_all") {
       try {
         var cmp_data = webhookData['click_tracking_id'];
         var campaign_data = JSON.parse(cmp_data);
         var vid = campaign_data.vid;
         var comp_id = campaign_data.comp_id;
         var camp_data = new Object(campaign_data.custom_params);
-        var event;
-        if (webhookData['status'] == 'success') {
-          event = "_email_delivered";
-        } 
+        var event = "_email_delivered"; 
         if (webhookData['event_type'] == "bounce_all"){
           event = "_email_bounce"
           camp_data["bounce_text"] = webhookData.bounce_text;
