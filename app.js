9298d501a7358c3425baa79b6689dce26c5d3310
harish <harish.chilamanthula@gmail.com>
2019-06-07 17:24:25 +0530

    dsfs

--- app.js
+++ app.js
@@ -513,7 +513,7 @@ app.post('/implapi', function (req, res) {
   //console.log("req", req);
   var data = req.body;
   console.log(typeof (data));
-  console.log(data[c]);
+  console.log(data['c']);
 
   res.writeHead(200);
   res.end("OK");
