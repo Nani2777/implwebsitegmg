c405ab4ee6102614bbba29e1dbfdcdc8b2744f54
harish <harish.chilamanthula@gmail.com>
2019-06-10 14:53:54 +0530

    changes

--- app.js
+++ app.js
@@ -501,27 +501,16 @@ app.post('/mlcwebhook', function (req, res) {
 
 app.post('/implapi', function (req, res) {
   try {
-    Logger.info('IMPL API logs');
-    console.log('IMPL API logs');
-    console.log('Body logs');
-    console.log(req.body);
-    console.log('Headers logs');
-    console.log(req.headers);
-    //Logger.info(req.body);
-    //Logger.info(req.query);
-    //Logger.info(req.params);
-    //Logger.info(req.headers);
-    //console.log("req", req);
     var data = req.body;
-    //console.log(typeof (data));
+    console.log(typeof (data));
     var comp_id = data['c'];
     var vid = data['v'];
     var event = data['e'];
     var custom_params = Object.keys(data).reduce((object, key) => {
-      if (key != "c" && key != "v" && key != "e") {
+    if (key != "c" && key != "v" && key != "e") {
         object[key] = data[key]
-      }
-      return object
+    }
+    return object
     }, {});
     var str_data = JSON.stringify(custom_params);
     var final_data = str_data.replace('{','').replace('}','').replace(/"/g,'').replace(/,/g,'&').replace(/:/g,'=');
@@ -529,12 +518,11 @@ app.post('/implapi', function (req, res) {
     try {
       var url = "http://evbk.gamooga.com/ev/?c="+ comp_id +"&v=" + vid + "&e=" + event +"&"+final_data
       /*Object.entries(custom_params).forEach(
-        ([key, value]) => url = url + "&ky=" + key + "&vl=" + value + "&tp=s"
+          ([key, value]) => url = url + "&ky=" + key + "&vl=" + value + "&tp=s"
       );*/
       console.log(url)
       axios.get(url).then(function (response) {}).catch(function (error) {
-        console.log(error);
-        Logger.error(error);
+          console.log(error);
       });
     } catch (err) {
       console.log('Error in Webhook from Gamooga Event API', err);
@@ -542,11 +530,10 @@ app.post('/implapi', function (req, res) {
       res.end("ERROR");
     }
   }
-  catch (err) {
-    //console.log('Wooplr Error in Webhook from Gupshups object \n%s', err);
-    Logger.error('Wooplr Error in Webhook from Gupshups object \n%s', err);
-    res.writeHead(200);
-    res.end("ERROR");
+    catch (err) {
+        console.log('Wooplr Error in Webhook from Gupshups object \n%s', err);
+        res.writeHead(200);
+        res.end("ERROR");
   }
   res.writeHead(200);
   res.end("OK");
