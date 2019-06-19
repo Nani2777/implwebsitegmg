a8d49017448ba86b30f9703fa7c8025f344e71e8
Jagapathi Babu <jagapathi.rgukt@gmail.com>
2019-06-19 14:52:23 +0530

    Done

--- app.js
+++ app.js
@@ -153,8 +153,58 @@ app.get("/gmgfalconide", function(req, res) {
 });
 
 app.post("/gmgnetcore", function(req, res) {
-  var webhookData = req.query;
-  console.log(webhookData);
+  try {
+    let data = req.body;
+    console.log(typeof data);
+    if (typeof data == "object") {
+      if (data.jobname && data.jobname !== "null") {
+        let div = data.jobname.split(",");
+        let params = {};
+        for (i = 0; i < div.length; i++) {
+          let fin = div[i].split(":");
+          params[fin[0]] = fin[1];
+        }
+        var custom_params = Object.keys(params).reduce((object, key) => {
+          if (key != "comp_id" && key != "vid") {
+            object[key] = params[key];
+          }
+          return object;
+        }, {});
+        var Server =
+          params.comp_id == "fcbe3928-6512-48a6-8cb5-c8c51e100539"
+            ? "js3in1.gamooga.com"
+            : "evbk.gamooga.com";
+        var url =
+          "http://" +
+          Server +
+          "/ev/?c=" +
+          params.comp_id +
+          "&v=" +
+          params.vid +
+          "&e=_sms_delivered";
+        Object.entries(custom_params).forEach(
+          ([key, value]) =>
+            (url = url + "&ky=" + key + "&vl=" + value + "&tp=s")
+        );
+        console.log(url);
+        axios
+          .get(url)
+          .then(function(response) {
+            console.log(response.statusText);
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      } else {
+        console.log("jobname was not paasing while sending sms");
+      }
+    }
+  } catch (err) {
+    console.log("Error in entries for MOSL req data", err);
+    res.writeHead(200);
+    res.end("ERROR");
+  }
+  res.end("OK");
 });
 app.post("/iflwebhook", function(req, res) {
   console.log(req.body);
